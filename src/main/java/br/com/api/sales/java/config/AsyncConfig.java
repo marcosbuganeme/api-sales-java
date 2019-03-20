c1c43ff014efe19e8a80f34649a45862ece500cc
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-19 23:59:18 -0300

    configurando async spring boot

---
+++ src/main/java/br/com/api/sales/java/config/AsyncConfig.java
@@ -0,0 +1,8 @@
+package br.com.api.sales.java.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {}
