e6ddda87caf1397c089c5a7a68c6fd04d731d347
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-19 23:44:56 -0300

    create service basic products

---
+++ src/main/java/br/com/api/sales/java/service/ProductService.java
@@ -0,0 +1,49 @@
+package br.com.api.sales.java.service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.api.sales.java.model.Product;
+import br.com.api.sales.java.repository.ProductJpaRepository;
+
+public @Service class ProductService {
+
+    private @Autowired ProductJpaRepository repository;
+
+    @Transactional
+    public Product create(Product product) {
+
+        validateBusiness(product);
+
+        return repository.save(product);
+    }
+
+    @Transactional
+    public Product update(Long id, Product product) {
+
+        Product productSearch = repository.findById(id).orElse(null);
+
+        Optional
+            .ofNullable(productSearch)
+            .filter(Objects::nonNull)
+            .orElseThrow( () -> new IllegalArgumentException("Product is invalid"));
+
+        return create(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    final void validateBusiness(Product product) {
+        Optional
+        .ofNullable(product)
+        .filter(Objects::nonNull)
+        .orElseThrow(() -> new IllegalArgumentException("Product invalid"));    
+    }
+}
