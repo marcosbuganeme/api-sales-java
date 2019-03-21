63006706023d95574d561cc6c5e94ca74e5b347e
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-21 20:53:31 -0300

    exception resource not found

---
+++ src/main/java/br/com/api/sales/java/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.api.sales.java.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
