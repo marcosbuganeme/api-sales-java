2a63bc47b79ba03142c0f57392ceee98c2eb3256
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-21 20:55:00 -0300

    remove @Validated for spring add @Valid javax

--- src/main/java/br/com/api/sales/java/controller/ProductController.java
+++ src/main/java/br/com/api/sales/java/controller/ProductController.java
@@ -1,8 +1,9 @@
 package br.com.api.sales.java.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,7 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,34 +30,26 @@ public class ProductController {
     private @Autowired ProductService service;
 
     @PostMapping("save")
-    @PreAuthorize("hasRole('ROLE_USER')")
-    public ResponseEntity<?> createProduct(@Validated @RequestBody Product product, UriComponentsBuilder uriBuilder) {
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> createProduct(@Valid @RequestBody Product product, UriComponentsBuilder uriBuilder) {
 
         Product createdProduct = service.create(product);
 
         URI location = uriBuilder
-                        .path("products/edit/{id}")
+                        .path("products/edit/{id:\\d+}")
                         .buildAndExpand(createdProduct.getId())
                         .toUri();
 
         return ResponseEntity.created(location).body(createdProduct);
     }
 
-    @PutMapping("edit/{id}")
-    public ResponseEntity<?> updateProduct(@PathVariable("id") Long id, @Validated @RequestBody Product product) {
+    @PutMapping("edit/{id:\\d+}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
 
-        Product productUpdate;
+        Product productUpdate = service.update(id, product);
 
-        try {
-
-            productUpdate = service.update(id, product);
-
-            return ResponseEntity.ok(productUpdate);
-
-        } catch (Exception exception) {
-
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(productUpdate);
     }
 
     @GetMapping("all")
