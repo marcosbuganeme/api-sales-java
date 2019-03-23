51e65d409ac0801287e252e89fd0489333b9618c
Marcos O. S. Buganeme <marcos.after@gmail.com>
2019-03-23 19:28:41 -0300

    reactory product controller

--- src/main/java/br/com/api/sales/java/controller/ProductController.java
+++ src/main/java/br/com/api/sales/java/controller/ProductController.java
@@ -1,7 +1,6 @@
 package br.com.api.sales.java.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,13 +21,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.api.sales.java.controller.shared.ControllerShared;
 import br.com.api.sales.java.model.Product;
 import br.com.api.sales.java.service.ProductService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("v1/products")
-public class ProductController {
+@Api("Endpoint for products version 1")
+public class ProductController extends ControllerShared<Product> {
 
     private @Autowired ProductService service;
 
@@ -53,9 +54,9 @@ public class ProductController {
     @ApiOperation(value = "Update product for id valid", response = Product.class)
     public ResponseEntity<?> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
 
-        Product productUpdate = service.update(id, product);
+        service.update(id, product);
 
-        return ResponseEntity.ok(productUpdate);
+        return ResponseEntity.ok(product);
     }
 
     @DeleteMapping("delete/{id:\\d+}")
@@ -74,9 +75,9 @@ public class ProductController {
         Page<Product> results = service.getAllProducts(pageable);
 
         return Optional
-                .ofNullable(results.getContent())
-                .filter(List::isEmpty)
-                .map(itens -> new ResponseEntity(results, HttpStatus.NOT_FOUND))
+                .ofNullable(results)
+                .filter(filterResultIsEmpty())
+                .map(mapResourceNotFound())
                 .orElse(ResponseEntity.ok(results));
     }
 }
