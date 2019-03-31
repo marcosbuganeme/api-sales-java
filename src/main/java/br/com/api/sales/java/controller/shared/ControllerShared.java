8d22b942b86ba39d9fae84ec7b02e85c9771e029
Marcos Buganeme <marcos.after@gmail.com>
2019-03-31 11:35:59 -0300

    refactory shared controller

--- src/main/java/br/com/api/sales/java/controller/shared/ControllerShared.java
+++ src/main/java/br/com/api/sales/java/controller/shared/ControllerShared.java
@@ -1,5 +1,6 @@
 package br.com.api.sales.java.controller.shared;
 
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -7,13 +8,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class ControllerShared<T> {
+public abstract class ControllerShared<T> {
 
-    protected Predicate<Page<T>> filterResultIsEmpty() {
+	protected Predicate<T> filterObjectIsNull() {
+		return Objects::isNull;
+	}
+
+    protected Predicate<Page<T>> filterListIsEmpty() {
         return page -> page.getContent().isEmpty();
     }
 
-    protected Function<Page<T>, ResponseEntity<Page<T>>> mapResourceNotFound() {
+    protected Function<T, ResponseEntity<T>> objectResourceNotFound() {
+        return object -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(object);
+    }
+
+    protected Function<Page<T>, ResponseEntity<Page<T>>> pageResourceNotFound() {
         return page -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(page);
     }
 }
