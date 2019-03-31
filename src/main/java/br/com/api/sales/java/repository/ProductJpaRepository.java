f3125e6cacd58d26317ff5092b71aaaed73b3647
Marcos Buganeme <marcos.after@gmail.com>
2019-03-30 21:36:02 -0300

    find product by name and price

--- src/main/java/br/com/api/sales/java/repository/ProductJpaRepository.java
+++ src/main/java/br/com/api/sales/java/repository/ProductJpaRepository.java
@@ -1,10 +1,14 @@
 package br.com.api.sales.java.repository;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.api.sales.java.model.Product;
 
 public @Repository interface ProductJpaRepository extends JpaRepository<Product, Long> {
-    
+
+	Optional<Product> findByNameAllIgnoreCaseAndPrice(String name, BigDecimal price);
 }
