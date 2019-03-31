593a1928ce2ff2baa56236ff9faa91bcb802a005
Marcos Buganeme <marcos.after@gmail.com>
2019-03-30 21:36:13 -0300

    model validation

--- src/main/java/br/com/api/sales/java/model/Product.java
+++ src/main/java/br/com/api/sales/java/model/Product.java
@@ -1,20 +1,19 @@
 package br.com.api.sales.java.model;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import br.com.api.sales.java.model.shared.DomainAbstract;
+
 @Entity
 @Table(name = "products")
-public class Product {
+public class Product extends DomainAbstract<Long> {
 
-    private Long id;
     private String name;
     private BigDecimal price;
 
@@ -27,23 +26,19 @@ public class Product {
         this.price = price;
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     @NotBlank(message = "Name is required")
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+
+        this.name = Optional
+        				.ofNullable(name)
+        				.filter(isNotBlank())
+        				.map(stripAccents())
+        				.map(toLowerCase())
+        				.orElse(null);
     }
 
     @NotNull(message = "Price is required")
