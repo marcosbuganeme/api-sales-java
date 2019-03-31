5c362a34a79459a8c4233ed6f0a57969707e0b72
Marcos Buganeme <marcos.after@gmail.com>
2019-03-30 21:35:21 -0300

    exception

--- src/main/java/br/com/api/sales/java/exception/ResourceDuplicateException.java
+++ src/main/java/br/com/api/sales/java/exception/ResourceDuplicateException.java
@@ -3,10 +3,10 @@ package br.com.api.sales.java.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ResourceDuplicateException extends RuntimeException {
     
-    public ResourceNotFoundException(String message) {
+    public ResourceDuplicateException(String message) {
         super(message);
     }
 }
